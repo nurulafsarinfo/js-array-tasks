58eb3df6ea2671356ea32d38a22ee012d9e29701
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-02-08 00:11:24 +0600

    complete 2nd task

---
+++ add-remove.js
@@ -0,0 +1,11 @@
+const destinations = ['rangamati', 'shilet', 'japlong'];
+destinations.push('shajek');
+console.log("All destinations: "+destinations);
+
+// add two more destination in array
+destinations.push('kuakata', 'saintmartin');
+console.log(destinations);
+
+// remove last tourist destination
+destinations.pop();
+console.log("Final Array is: "+destinations);
