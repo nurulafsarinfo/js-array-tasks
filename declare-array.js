b56f8fc0042b416c2cd56477d25d516b0c573dba
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-02-08 00:02:32 +0600

    complete task 1

--- declare-array.js
+++ declare-array.js
@@ -1,1 +1,6 @@
-const fruits = ['apple', 'banana', 'coconut', 'pump', 'watermilion']
+const fruits = ['apple', 'banana', 'coconut', 'pump', 'watermilion'];
+
+console.log("Third index element is: " +fruits[2]);
+
+fruits[2] = 'jambura';
+console.log("final array is : "+ fruits)
