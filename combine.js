1e43cbc7b98880c0df4ff3c23377f3d844520a33
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-02-08 10:18:44 +0600

    Complete combine array task

---
+++ combine.js
@@ -0,0 +1,5 @@
+const boys = ['Rohim', 'Korim', 'Rasel'];
+const girls = ['Mita', 'Fariha', 'Tasnim'];
+
+const Student = boys.concat(girls);
+console.log("First original array is: "+boys +"\nAnd Second original array is: "+girls+ "\nStudent name after Concating ->"+Student);
