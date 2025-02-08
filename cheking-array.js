d4246a30e0619ad6439fc36fca89375a0480dbe2
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-02-08 09:48:06 +0600

    checking includes element in array taskcompleted

---
+++ cheking-array.js
@@ -0,0 +1,10 @@
+const books = ['python', 'javascript', 'c++', 'php'];
+
+console.log(books.includes('javascript'));
+
+if(books.includes("javascript")){
+    console.log('The element is present in the array.')
+}
+else{
+    console.log('Not present in this array')
+}
