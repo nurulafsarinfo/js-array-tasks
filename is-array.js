5d761a6c0d369036e0a7de1413098c69a990ac4a
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-02-08 10:06:35 +0600

    complete isArray checking task

---
+++ is-array.js
@@ -0,0 +1,14 @@
+const destinations = ['rangamati', 'shilet', 'japlong'];
+
+const books = ['python', 'javascript', 'c++', 'php'];
+
+const fruits = [];
+
+const tree = "Mango tree Apple tree";
+
+const num = 12455623;
+
+console.log("Is this a 'Array': "+Array.isArray(destinations))
+console.log("Is fruits a 'Array': "+Array.isArray(fruits))
+console.log("Is this tree a 'Array': "+Array.isArray(tree))
+console.log("Is this a 'Array': "+Array.isArray(books))
